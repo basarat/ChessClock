5d9f1eb98350662d89b91e18c5835c2c08502f25
basarat <basaratali@gmail.com>
2013-01-20 14:39:56 +1100

    Completely fluid times and buttons

--- ChessClock/ChessClock/www/Scripts/jquery.fittext.js
+++ ChessClock/ChessClock/www/Scripts/jquery.fittext.js
@@ -23,7 +23,8 @@
         var compressor = kompressor || 1,
             settings = $.extend({
                 'minFontSize': Number.NEGATIVE_INFINITY,
-                'maxFontSize': Number.POSITIVE_INFINITY
+                'maxFontSize': Number.POSITIVE_INFINITY,
+                'basedon': '' //resize this based on another items height / width
             }, options);
 
         return this.each(function () {
@@ -34,10 +35,35 @@
             // Resizer() resizes items based on the object width divided by the compressor * 10
             var resizer = function () {
                 //CUSTOMCODE
-                var sizew = Math.max(Math.min($this.width() / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize));
+                //check if it is visible if not. Just return since no point in resizing: 
+                if (!$this.is(":visible"))
+                    return;
+
+                var height = $this.height();
+                var width = $this.width();
+                if (settings.basedon) {
+                    var height = $(settings.basedon).height();
+                    var width = $(settings.basedon).width();
+                }
+
+                var sizew = Math.max(Math.min(width / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize));
                 //use height as well 
-                var sizeh = Math.max(Math.min($this.height(), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize));                     
+                var sizeh = Math.max(Math.min(height, parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize));
+
+                console.log(height, width, sizeh, sizew);
+
+                //If now width or height is set. Ignore that dimension:
+                if (height == 0) {
+                    sizeh = Number.POSITIVE_INFINITY;
+                }
+                if ((width == 0)) {
+                    sizew = Number.POSITIVE_INFINITY;
+
+                }
+                //Take the min of both dimension calculations
                 size = Math.min(sizew, sizeh)
+
+
 
                 $this.css('font-size', size);
                 //Setup line-height as well required because of bootstrap: 
@@ -52,9 +78,9 @@
             // Call on resize. Opera debounces their resize by default. 
             $(window).on('resize', resizer);
 
-            //CUSTOMCODE
-            setInterval(function () { resizer() }, 1000);
-            //CUSTOMCODE
+            ////CUSTOMCODE
+            //setInterval(function () { resizer() }, 1000);
+            ////CUSTOMCODE
 
         });
 
