52ab0507924160e06565e75ec26d472dacd61064
basarat <basaratali@gmail.com>
2013-01-20 14:09:58 +1100

    Added resizing of buttons text based on width AND height of current view

---
+++ ChessClock/ChessClock/www/Scripts/jquery.fittext.js
@@ -0,0 +1,63 @@
+/*global jQuery */
+/*!	
+* FitText.js 1.1
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license 
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu May 05 14:23:00 2011 -0600
+*/
+
+
+
+
+
+(function ($) {
+
+
+
+    $.fn.fitText = function (kompressor, options) {
+
+        // Setup options
+        var compressor = kompressor || 1,
+            settings = $.extend({
+                'minFontSize': Number.NEGATIVE_INFINITY,
+                'maxFontSize': Number.POSITIVE_INFINITY
+            }, options);
+
+        return this.each(function () {
+
+            // Store the object
+            var $this = $(this);
+
+            // Resizer() resizes items based on the object width divided by the compressor * 10
+            var resizer = function () {
+                //CUSTOMCODE
+                var sizew = Math.max(Math.min($this.width() / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize));
+                //use height as well 
+                var sizeh = Math.max(Math.min($this.height(), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize));                     
+                size = Math.min(sizew, sizeh)
+
+                $this.css('font-size', size);
+                //Setup line-height as well required because of bootstrap: 
+                //NOTE We set it to height as assumed by sizeh :) 
+                $this.css('line-height', sizeh + 'px');
+                //CUSTOMCODE
+            };
+
+            // Call once to set.
+            resizer();
+
+            // Call on resize. Opera debounces their resize by default. 
+            $(window).on('resize', resizer);
+
+            //CUSTOMCODE
+            setInterval(function () { resizer() }, 1000);
+            //CUSTOMCODE
+
+        });
+
+    };
+
+})(jQuery);
