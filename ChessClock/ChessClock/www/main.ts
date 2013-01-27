426c114e63faca48382d735dbcab455b396022ac
basarat <basaratali@gmail.com>
2013-01-27 21:34:25 +1100

    Removed knockout and working on porting to AngularJS. Have the interactions working. Need to work on selecting css classes / visibility with angular instead of knockout.

--- ChessClock/ChessClock/www/main.ts
+++ ChessClock/ChessClock/www/main.ts
@@ -1,43 +1,5 @@
+/// <reference path="scripts/angular/angular.d.ts" />
 /// <reference path="Scripts/jquery/jquery.d.ts" />
-/// <reference path="Scripts/knockout/knockout.d.ts" />
-
-//A numeric display extender for knockout: 
-interface KnockoutExtenders {
-    numeric(target: any, precision: number): KnockoutObservableAny;
-}
-interface KnockoutObservableNumber {
-    extend(data: any): KnockoutObservableNumber;
-}
-ko.extenders.numeric = function (target: KnockoutObservableNumber, digits) {
-    var result = ko.computed({
-        read: function () {
-            var value = target();
-            var toret: string = value.toString();
-            if (toret.length < digits) {
-                toret = "0" + toret;
-            }
-            else if (toret.length > digits) {
-                toret = toret.substring(0, digits);
-            }
-            return toret;
-        },
-        write: target
-    });
-
-    result(target());
-    return result;
-};
-
-//http://stackoverflow.com/questions/10114472/is-it-possible-to-data-bind-visible-the-not-of-a-boolean-viewmodel-property
-(<any>(ko.bindingHandlers)).hidden = {
-  update: function(element, valueAccessor) {
-    (<any>(ko.bindingHandlers.visible)).update(element, function() {
-      !ko.utils.unwrapObservable(valueAccessor());
-      console.log("hidden was asked to be updated");
-    });
-  }
-};
-
 
 //Game states: 
 class GameStates {
@@ -51,15 +13,15 @@ class GameStates {
 //a class that self contains mins/sec/miliseconds 
 //and supports decrementing these based on passed in milliseconds
 class Player {
-    min = ko.observable(0).extend({ numeric: 2 });
-    sec = ko.observable(0).extend({ numeric: 2 });
-    mil = ko.observable(0).extend({ numeric: 1 });
+    min = 0;
+    sec = 0;
+    mil = 0;
 
     decreaseTime(milliseconds: number) {
         //orig
-        var min = this.min();
-        var sec = this.sec();
-        var mil = this.mil();
+        var min = this.min;
+        var sec = this.sec;
+        var mil = this.mil;
 
         var totalTime = this.totalTime();
 
@@ -78,16 +40,16 @@ class Player {
         }
 
         //Update the ui
-        this.min(min);
-        this.sec(sec);
-        this.mil(mil);
+        this.min = (min);
+        this.sec = (sec);
+        this.mil = (mil);
     }
 
     totalTime(): number {
         //orig
-        var min = this.min();
-        var sec = this.sec();
-        var mil = this.mil();
+        var min = this.min;
+        var sec = this.sec;
+        var mil = this.mil;
 
         var minConv = 60 * 1000; //milliseconds 
         var secConv = 1000; //milliseconds 
@@ -96,93 +58,100 @@ class Player {
     }
 
     setTime(min: number, sec: number, mil: number) {
-        this.min(min);
-        this.sec(sec);
-        this.mil(mil);
+        this.min = (min);
+        this.sec = (sec);
+        this.mil = (mil);
     }
 }
 
+//This seems the best way to expose member variables 
+//Name 
+//    your class as <ClassController> and 
+//    have a corresponsding scope definition named <IClassScope>
+interface IMainScope extends ng.IScope {
+    p1: Player;
+    p2: Player;
+    gameState: number;
+
+    //functions: 
+    reset();
+    p1turn();
+    p2turn();
+    play_pause();
+
+}
+
+
 //ViewModel class 
-class ViewModel {
+class MainController {
     //The miliseconds used to control key game times 
     gameTime = 100;//milliseconds 
-    p1: Player = new Player();
-    p2: Player = new Player();
-    //Current game state
-    gameState = ko.observable(GameStates.paused);
+
     //For resuming 
     lastPlayer = GameStates.p1turn;
-    //A computed observable for play pause visibility
-    IsPaused: KnockoutComputed;
 
-    constructor() {
+    constructor($scope: IMainScope) {
+        //players
+        $scope.p1 = new Player();
+        $scope.p2 = new Player();
+        //Current game state
+        $scope.gameState = GameStates.paused;
+
+
+        //setup the functions 
+        $scope.p1turn = () => { $scope.gameState = (GameStates.p1turn); };
+        $scope.p2turn = () => { $scope.gameState = (GameStates.p2turn); };
+        $scope.reset = () => {
+            this.lastPlayer = GameStates.p1turn;
+            $scope.gameState = (GameStates.paused);
+            $scope.p1.setTime(1, 15, 0);
+            $scope.p2.setTime(1, 15, 20);
+        }
+        $scope.play_pause = () => {
+            if ($scope.gameState != GameStates.paused) {
+                //We need to remember the current player whose turn we are pausing on 
+                this.lastPlayer = $scope.gameState;
+                $scope.gameState = (GameStates.paused);
+            }
+            else {
+                $scope.gameState = (this.lastPlayer);
+            }
+        };
+
+
         //Loop for every 100ms: 
         var self = this;
-        setInterval(function () { self.update() }, this.gameTime);
-
-        //computed observables:
-        this.IsPaused = ko.computed(() => { return (this.gameState() == GameStates.paused); });
-        this.IsPaused.subscribe((val) => { console.log(val); });
+        setInterval(function () { $scope.$apply(function () { self.update($scope) }) }, this.gameTime);
 
         //initial setup:
-        this.reset();
+        $scope.reset();
 
         //DEBUG for testing: 
         //for game state
-        this.gameState(GameStates.p1turn);
+        $scope.gameState = (GameStates.p1turn);
         //for timers
-        this.p1.setTime(1, 15, 0);
-        this.p2.setTime(0, 10, 20);
+        $scope.p1.setTime(1, 15, 0);
+        $scope.p2.setTime(0, 10, 20);
 
     }
 
     //Main loop to take actions based on current game state
-    update(): any {
-        switch (this.gameState()) {
+    update($scope: IMainScope): any {
+        switch ($scope.gameState) {
             case GameStates.paused:
                 break;
             case GameStates.p1turn:
-                this.p1.decreaseTime(this.gameTime);
-                if (this.p1.totalTime() == 0)
-                    this.gameState(GameStates.p2win);
+                $scope.p1.decreaseTime(this.gameTime);
+                if ($scope.p1.totalTime() == 0)
+                    $scope.gameState = (GameStates.p2win);
                 break;
             case GameStates.p2turn:
-                this.p2.decreaseTime(this.gameTime);
-                if (this.p2.totalTime() == 0)
-                    this.gameState(GameStates.p1win);
+                $scope.p2.decreaseTime(this.gameTime);
+                if ($scope.p2.totalTime() == 0)
+                    $scope.gameState = (GameStates.p1win);
                 break;
         }
-        
     }
-
-    reset() {
-        this.lastPlayer = GameStates.p1turn;
-        this.gameState(GameStates.paused);
-        this.p1.setTime(1, 15, 0);
-        this.p2.setTime(1, 15, 20);
-    }
-
-    p1turn() {
-        this.gameState(GameStates.p1turn);
-    }
-
-    p2turn() {
-        this.gameState(GameStates.p2turn);
-    }
-
-    play_pause() {
-        if (this.gameState() != GameStates.paused) {
-            //We need to remember the current player whose turn we are pausing on 
-            this.lastPlayer = this.gameState();
-            this.gameState(GameStates.paused);
-        }
-        else{
-            this.gameState(this.lastPlayer);
-        }
-    }
-    
-
-
 }
 
 
