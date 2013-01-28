f0e876b36cccae953acc5b381b3507e7a27c0972
basarat <basaratali@gmail.com>
2013-01-28 14:37:39 +1100

    Added an application module to angular js to create a filter that allows making custom number of digits display from a number

---
+++ ChessClock/ChessClock/www/Scripts/angular.filters.js
@@ -0,0 +1,20 @@
+﻿
+angular.module('filters', []).
+/**
+ * Format Number digits
+ * @Param num
+ * @Param length, 2 
+ * @return string
+ */
+    filter('digits', function () {
+        return function (num, length) {
+            if (isNaN(length)) {
+                length = 2;
+            }
+            num = num.toString();
+            while (num.length < length) {
+                num = "0" + num;                
+            }
+            return num;
+        };
+    });
